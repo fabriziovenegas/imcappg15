5e9972a1410deb681753237047d7554b1913e5e4
fabriziovenegas <fabrizio.adrian.venegas@gmail.com>
2026-05-05 11:31:28 -0500

    Altura realizada en metros

--- lib/pages/imc_page.dart
+++ lib/pages/imc_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
-class ImcPage extends StatelessWidget {
+class ImcPage extends StatefulWidget {
+  @override
+  State<ImcPage> createState() => _ImcPageState();
+}
+
+class _ImcPageState extends State<ImcPage> {
   double altura = 0.5, peso = 40;
 
-  Widget _buildSlider(String title, double value, double min, double max) {
+  Widget _buildSlider(
+    String title,
+    double value,
+    double min,
+    double max,
+    Function(double) onChanged,
+  ) {
     return Column(
       children: [
         Text(title),
-        Slider(min: min, max: max, value: value, onChanged: (double valor) {}),
+        RichText(
+          text: TextSpan(
+            text: value.toStringAsFixed(1),
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            children: [TextSpan(text: "m.", style: TextStyle(fontSize: 20))],
+          ),
+        ),
+
+        Slider(min: min, max: max, value: value, onChanged: onChanged),
       ],
     );
   }
@@ -24,7 +47,12 @@ class ImcPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [_buildSlider("Altura", altura, 0.4, 2.1)],
+          children: [
+            _buildSlider("Altura", altura, 0.4, 2.1, (v) {
+              altura = v;
+              setState(() {});
+            }),
+          ],
         ),
       ),
     );
