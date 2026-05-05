b35015d1b36dc9aa5950fe74308daf9fcbd79f6e
fabriziovenegas <fabrizio.adrian.venegas@gmail.com>
2026-05-05 13:32:07 -0500

    App casi terminada

--- lib/pages/imc_page.dart
+++ lib/pages/imc_page.dart
@@ -7,30 +7,46 @@ class ImcPage extends StatefulWidget {
 
 class _ImcPageState extends State<ImcPage> {
   double altura = 0.5, peso = 40;
+  double imcResult = 0;
 
-  Widget _buildSlider(
-    String title,
-    double value,
-    double min,
-    double max,
-    Function(double) onChanged,
-  ) {
+  double calcularIMC() {
+    double calculo = peso / (altura * altura);
+    calculo = double.parse(calculo.toStringAsFixed(2));
+    print(calculo);
+    return calculo;
+  }
+
+  Widget _buildSlider({
+    required double value,
+    required bool isAltura,
+    required Function(double) onChanged,
+  }) {
     return Column(
       children: [
-        Text(title),
+        Text(isAltura ? "Altura" : "Peso"),
         RichText(
           text: TextSpan(
             text: value.toStringAsFixed(1),
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 40,
               fontWeight: FontWeight.bold,
               color: Colors.black,
             ),
-            children: [TextSpan(text: "m.", style: TextStyle(fontSize: 20))],
+            children: [
+              TextSpan(
+                text: "${isAltura ? " m " : " kg "}.",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
           ),
         ),
 
-        Slider(min: min, max: max, value: value, onChanged: onChanged),
+        Slider(
+          min: isAltura ? 0.4 : 40,
+          max: isAltura ? 2.1 : 130,
+          value: value,
+          onChanged: onChanged,
+        ),
       ],
     );
   }
@@ -48,10 +64,45 @@ class _ImcPageState extends State<ImcPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _buildSlider("Altura", altura, 0.4, 2.1, (v) {
-              altura = v;
-              setState(() {});
-            }),
+            _buildSlider(
+              isAltura: true,
+              value: altura,
+              onChanged: (v) => setState(() {
+                altura = v;
+              }),
+            ),
+            _buildSlider(
+              value: peso,
+              isAltura: false,
+              onChanged: (v) => setState(() {
+                peso = v;
+              }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    imcResult = calcularIMC();
+                    setState(() {});
+                  },
+                  child: Text("Calcular"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff248CFA),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              imcResult.toString(),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
